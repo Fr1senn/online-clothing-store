0eadbe1745d1981c2a75fa4fe257f9790c3585d9
Vlad Bomba <bombavlad1@gmail.com>
2024-03-01 10:48:06 +0200

    Initial migration has been created

---
+++ Migrations/20240301084559_InitialMigration.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace OnlineClothingStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("categories_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "discounts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    percentage = table.Column<int>(type: "integer", nullable: false),
+                    begin_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    end_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    is_acitive = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("discounts_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "manufacturers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("manufacturers_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("roles_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "statuses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("statuses_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    price = table.Column<decimal>(type: "numeric(7,2)", precision: 7, scale: 2, nullable: false),
+                    rating = table.Column<int>(type: "integer", nullable: true),
+                    manufacturer_id = table.Column<int>(type: "integer", nullable: false),
+                    category_id = table.Column<int>(type: "integer", nullable: false),
+                    discount_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("products_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "products_category_id_fkey",
+                        column: x => x.category_id,
+                        principalTable: "categories",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "products_discount_id_fkey",
+                        column: x => x.discount_id,
+                        principalTable: "discounts",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "products_manufacturer_id_fkey",
+                        column: x => x.manufacturer_id,
+                        principalTable: "manufacturers",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    first_name = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    last_name = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    birth_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    registration_date = table.Column<DateOnly>(type: "date", nullable: true, defaultValueSql: "CURRENT_DATE"),
+                    email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    address = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    hashed_password = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    role_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "users_role_id_fkey",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    order_date = table.Column<DateOnly>(type: "date", nullable: false, defaultValueSql: "CURRENT_DATE"),
+                    status_id = table.Column<int>(type: "integer", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("orders_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "orders_status_id_fkey",
+                        column: x => x.status_id,
+                        principalTable: "statuses",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "orders_user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "reviews",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    review = table.Column<string>(type: "text", nullable: true),
+                    review_date = table.Column<DateOnly>(type: "date", nullable: false, defaultValueSql: "CURRENT_DATE"),
+                    rating = table.Column<int>(type: "integer", nullable: true),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    product_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("reviews_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "reviews_product_id_fkey",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "reviews_user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "wish_list",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    product_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("wish_list_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "wish_list_product_id_fkey",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "wish_list_user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order_items",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    quantity = table.Column<int>(type: "integer", nullable: false, defaultValue: 1),
+                    order_id = table.Column<int>(type: "integer", nullable: false),
+                    product_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("order_items_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "order_items_order_id_fkey",
+                        column: x => x.order_id,
+                        principalTable: "orders",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "order_items_product_id_fkey",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "categories_title_key",
+                table: "categories",
+                column: "title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "manufacturers_title_key",
+                table: "manufacturers",
+                column: "title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_items_order_id",
+                table: "order_items",
+                column: "order_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_items_product_id",
+                table: "order_items",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_status_id",
+                table: "orders",
+                column: "status_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_user_id",
+                table: "orders",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_category_id",
+                table: "products",
+                column: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_discount_id",
+                table: "products",
+                column: "discount_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_manufacturer_id",
+                table: "products",
+                column: "manufacturer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reviews_product_id",
+                table: "reviews",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reviews_user_id",
+                table: "reviews",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "roles_title_key",
+                table: "roles",
+                column: "title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "statuses_title_key",
+                table: "statuses",
+                column: "title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_role_id",
+                table: "users",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "users_email_key",
+                table: "users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_wish_list_product_id",
+                table: "wish_list",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_wish_list_user_id",
+                table: "wish_list",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "order_items");
+
+            migrationBuilder.DropTable(
+                name: "reviews");
+
+            migrationBuilder.DropTable(
+                name: "wish_list");
+
+            migrationBuilder.DropTable(
+                name: "orders");
+
+            migrationBuilder.DropTable(
+                name: "products");
+
+            migrationBuilder.DropTable(
+                name: "statuses");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+
+            migrationBuilder.DropTable(
+                name: "discounts");
+
+            migrationBuilder.DropTable(
+                name: "manufacturers");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+        }
+    }
+}
