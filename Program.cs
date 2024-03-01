4c40ca5b19b5b5031e8d5a4c3f7f7b375f55ecff
Vlad Bomba <bombavlad1@gmail.com>
2024-03-01 10:13:29 +0200

    Database context file has been injected

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
+using OnlineClothingStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<OnlineClothingStoreContext>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
