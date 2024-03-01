07043c9cebdf3d18cfd8c746d5dec18f767de62a
Vlad Bomba <bombavlad1@gmail.com>
2024-03-01 15:21:19 +0200

    Added migration file with changes to fields in the reviews table

---
+++ Migrations/20240301132041_ReviewColumnsChanges.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineClothingStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReviewColumnsChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "review",
+                table: "reviews",
+                newName: "comment");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "rating",
+                table: "reviews",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "comment",
+                table: "reviews",
+                newName: "review");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "rating",
+                table: "reviews",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
