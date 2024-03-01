36a9c186407440a8fa27676f009d5e70ecfd8874
Vlad Bomba <bombavlad1@gmail.com>
2024-03-01 14:18:28 +0200

    Added a new migration file that fixes a new field in order_items

---
+++ Migrations/20240301120719_OrderItemsNewColumn.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineClothingStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderItemsNewColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "price_at_order",
+                table: "order_items",
+                type: "numeric(7,2)",
+                precision: 7,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "price_at_order",
+                table: "order_items");
+        }
+    }
+}
